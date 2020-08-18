a79a6cc546fd47af3edbd966a33776e377cd4c06
Giacomo Randazzo <giacomoran@gmail.com>
2020-08-18 15:35:47 +0200

    Depend on remote leveldb-android library

--- testapp/android/app/src/main/java/com/testapp/MainApplication.java
+++ testapp/android/app/src/main/java/com/testapp/MainApplication.java
@@ -8,7 +8,6 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.reactnativeleveldown.LeveldownPackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -26,7 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          packages.add(new LeveldownPackage());
+          // packages.add(new LeveldownPackage());
           return packages;
         }
 
